4a4459780623ef97fec6dee6ccf41cde4c43983f
HAL <helenamelia23@gmail.com>
2019-12-06 15:45:10 -0800

    add env

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var express = require("express"),
     campgroundRoutes = require("./routes/campgrounds"),
     authenticationRoutes = require("./routes/index");
 
-mongoose.connect("mongodb+srv://hal:obisally23@yelpcampycluster-xstbo.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true, 
     useFindAndModify: false, 
     useUnifiedTopology: true});
